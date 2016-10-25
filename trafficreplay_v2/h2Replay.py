7d4f95149fa0cd79978c7914c4442e586341396d
Syeda Persia Aziz <persia@yahoo-inc.com>
2016-10-25 10:48:03 -0700

    h2

--- trafficreplay_v2/h2Replay.py
+++ trafficreplay_v2/h2Replay.py
@@ -69,18 +69,20 @@ def txn_replay(session_filename, txn, proxy, result_queue, h2conn,request_IDs):
         if 'Content-Length' in txn_req_headers_dict:
             nBytes=int(txn_req_headers_dict['Content-Length'])
             mbody = createDummyBodywithLength(nBytes)
+        if 'Connection' in txn_req_headers_dict:
+            del txn_req_headers_dict['Connection']
         str2 = extractHeader.extract_host(txn_req_headers)+ extractHeader.extract_GET_path(txn_req_headers)
         print(str2)
         if method == 'GET':
             #,'Connection': 'Keep-Alive','Cache-Control': 'no-cache'
             #'Accept': '*/*','Referer': 'https://www.yahoo.com','Accept-Language': 'en-US', 'Host': 'video-api.yql.yahoo.com','User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko/20100101 Firefox/12.0','DNT': '1',
             #,'Origin': 'https://www.yahoo.com','Cookie': 'B=64jquc99p42n7&b=4&d=enp9TGNpYEIKTob5dtylGwwB7oOcBLfDRVt4hg--&s=ih&i=Ie61vTBrDuG24oybu9GW; AO=u=1&o=0; F=a=344VOSMMvSvdYQM3vP0tBeijN5LduAtdRsK9TFCaUoDq2cACSB3umQrggAoTT_afLT2oqhCYAKybAUiwloRNtIDniQ--&b=_4ns&d=7.AUgqg9vPtCmTqqtC2UUunc6lCjVyH60u37M9dvlhg-; U=mt=INdsq52MhY9Nbg0xoxhmNfMFSj_DUELVVlSPQVs-&ux=M5rpWB&un=agcdaj3apu8q5; YLS=v=1&p=1&n=0; Y=v=1&n=agcdaj3apu8q5&l=20hd8j07eb82/o&p=f242rsr353000100&r=2s&lg=&intl=us; ucs=bnas=0&sfcTs=1426178993&sfc=1&fs=1&lnct=1440457481&tr=1440606784291; s_pers=%20s_c20%3D1406654291912%7C1501262291912%3B%20s_c20_s%3DLess%2520than%25207%2520days%7C1406656091912%3B%20s_gpv_pn%3DGMAY%253AAndi_Dorfman%253AAndi%2520Dorfman%2520and%2520Josh%2520Murray%2520Share%2520Details%2520of%2520Their%2520Fantasy%2520Suite%2520Stay%7C1406656091920%3B; YP=v=AwAAY&d=AEkAMEYCIQCttOGs9cSYOmSscZdWu4ygUhRaR1H1JBYvK.SNCy6aJgIhAMkqMJbiwGVVOMGwoWPb0SxqBL4i.NpebT6LkbxPu4PvAA--; PH=fn=bCJag48xXD1PllyIo9GFPg--&i=us; SSL=v=1&s=K9Qm.kKvPM4anlnP8YzsU8fAimEeiuItWE_fG92IYTzm_qfJCdoeTIyfgGMvhPCiNbeCR042n9CDUnZ0ZzK_ww--&kv=0; T=z=GZjmXBGtKrXBWBxWKR2vEXfNjM3NwYzNzY3MzcyTk4-&a=YAE&sk=DAAstXw5JWiCVT&ks=EAA3iopIpqeJDSb4BPFsuBmTQ--~E&kt=EAA6h3_gtnHDO8ZX26hL.59JQ--~F&d=c2wBTVRRd01BRTBNREV3TkRBMU9Uay0BYQFZQUUBZwFXWlBEU0UyTkFQTjVQR0tUTVZaWlFMQTM0VQFzY2lkAWlvS1Rsc2c0cVVMdlFkQjFMc29zc3ltLmppZy0BYWMBQU56M0ZCb0MBb2sBWlcwLQF0aXABZ1oxcXBEAXNjAWRlc2t0b3Bfd2ViAWZzAUFOR25IdlZTelpQbAF6egFHWmptWEJBN0U-; DNT=1'
-            #'Accept-Encoding': 'gzip, deflate',ß
-            realhdr = {'Host':'www.blabla.com','Cache-Control': 'no-cache','Origin': 'https://www.yahoo.com','Cookie': 'B=64jquc99p42n7&b=4&d=enp9TGNpYEIKTob5dtylGwwB7oOcBLfDRVt4hg--&s=ih&i=Ie61vTBrDuG24oybu9GW; AO=u=1&o=0; F=a=344VOSMMvSvdYQM3vP0tBeijN5LduAtdRsK9TFCaUoDq2cACSB3umQrggAoTT_afLT2oqhCYAKybAUiwloRNtIDniQ--&b=_4ns&d=7.AUgqg9vPtCmTqqtC2UUunc6lCjVyH60u37M9dvlhg-; U=mt=INdsq52MhY9Nbg0xoxhmNfMFSj_DUELVVlSPQVs-&ux=M5rpWB&un=agcdaj3apu8q5; YLS=v=1&p=1&n=0; Y=v=1&n=agcdaj3apu8q5&l=20hd8j07eb82/o&p=f242rsr353000100&r=2s&lg=&intl=us; ucs=bnas=0&sfcTs=1426178993&sfc=1&fs=1&lnct=1440457481&tr=1440606784291; s_pers=%20s_c20%3D1406654291912%7C1501262291912%3B%20s_c20_s%3DLess%2520than%25207%2520days%7C1406656091912%3B%20s_gpv_pn%3DGMAY%253AAndi_Dorfman%253AAndi%2520Dorfman%2520and%2520Josh%2520Murray%2520Share%2520Details%2520of%2520Their%2520Fantasy%2520Suite%2520Stay%7C1406656091920%3B; YP=v=AwAAY&d=AEkAMEYCIQCttOGs9cSYOmSscZdWu4ygUhRaR1H1JBYvK.SNCy6aJgIhAMkqMJbiwGVVOMGwoWPb0SxqBL4i.NpebT6LkbxPu4PvAA--; PH=fn=bCJag48xXD1PllyIo9GFPg--&i=us; SSL=v=1&s=K9Qm.kKvPM4anlnP8YzsU8fAimEeiuItWE_fG92IYTzm_qfJCdoeTIyfgGMvhPCiNbeCR042n9CDUnZ0ZzK_ww--&kv=0; T=z=GZjmXBGtKrXBWBxWKR2vEXfNjM3NwYzNzY3MzcyTk4-&a=YAE&sk=DAAstXw5JWiCVT&ks=EAA3iopIpqeJDSb4BPFsuBmTQ--~E&kt=EAA6h3_gtnHDO8ZX26hL.59JQ--~F&d=c2wBTVRRd01BRTBNREV3TkRBMU9Uay0BYQFZQUUBZwFXWlBEU0UyTkFQTjVQR0tUTVZaWlFMQTM0VQFzY2lkAWlvS1Rsc2c0cVVMdlFkQjFMc29zc3ltLmppZy0BYWMBQU56M0ZCb0MBb2sBWlcwLQF0aXABZ1oxcXBEAXNjAWRlc2t0b3Bfd2ViAWZzAUFOR25IdlZTelpQbAF6egFHWmptWEJBN0U-; DNT=1'}
-            dummyhdr={'Host':'www.blabla.com','Connection':'Keep-Alive'}
-            responseID = h2conn.request('GET',url=''+txn._uuid,
-                                    headers=realhdr)
-            print("get reso", responseID)
+            #'Accept-Encoding': 'gzip, deflate',
+            #realhdr = {'Host':'www.blabla.com','Cache-Control': 'no-cache','Origin': 'https://www.yahoo.com','Cookie': 'B=64jquc99p42n7&b=4&d=enp9TGNpYEIKTob5dtylGwwB7oOcBLfDRVt4hg--&s=ih&i=Ie61vTBrDuG24oybu9GW; AO=u=1&o=0; F=a=344VOSMMvSvdYQM3vP0tBeijN5LduAtdRsK9TFCaUoDq2cACSB3umQrggAoTT_afLT2oqhCYAKybAUiwloRNtIDniQ--&b=_4ns&d=7.AUgqg9vPtCmTqqtC2UUunc6lCjVyH60u37M9dvlhg-; U=mt=INdsq52MhY9Nbg0xoxhmNfMFSj_DUELVVlSPQVs-&ux=M5rpWB&un=agcdaj3apu8q5; YLS=v=1&p=1&n=0; Y=v=1&n=agcdaj3apu8q5&l=20hd8j07eb82/o&p=f242rsr353000100&r=2s&lg=&intl=us; ucs=bnas=0&sfcTs=1426178993&sfc=1&fs=1&lnct=1440457481&tr=1440606784291; s_pers=%20s_c20%3D1406654291912%7C1501262291912%3B%20s_c20_s%3DLess%2520than%25207%2520days%7C1406656091912%3B%20s_gpv_pn%3DGMAY%253AAndi_Dorfman%253AAndi%2520Dorfman%2520and%2520Josh%2520Murray%2520Share%2520Details%2520of%2520Their%2520Fantasy%2520Suite%2520Stay%7C1406656091920%3B; YP=v=AwAAY&d=AEkAMEYCIQCttOGs9cSYOmSscZdWu4ygUhRaR1H1JBYvK.SNCy6aJgIhAMkqMJbiwGVVOMGwoWPb0SxqBL4i.NpebT6LkbxPu4PvAA--; PH=fn=bCJag48xXD1PllyIo9GFPg--&i=us; SSL=v=1&s=K9Qm.kKvPM4anlnP8YzsU8fAimEeiuItWE_fG92IYTzm_qfJCdoeTIyfgGMvhPCiNbeCR042n9CDUnZ0ZzK_ww--&kv=0; T=z=GZjmXBGtKrXBWBxWKR2vEXfNjM3NwYzNzY3MzcyTk4-&a=YAE&sk=DAAstXw5JWiCVT&ks=EAA3iopIpqeJDSb4BPFsuBmTQ--~E&kt=EAA6h3_gtnHDO8ZX26hL.59JQ--~F&d=c2wBTVRRd01BRTBNREV3TkRBMU9Uay0BYQFZQUUBZwFXWlBEU0UyTkFQTjVQR0tUTVZaWlFMQTM0VQFzY2lkAWlvS1Rsc2c0cVVMdlFkQjFMc29zc3ltLmppZy0BYWMBQU56M0ZCb0MBb2sBWlcwLQF0aXABZ1oxcXBEAXNjAWRlc2t0b3Bfd2ViAWZzAUFOR25IdlZTelpQbAF6egFHWmptWEJBN0U-; DNT=1'}
+            #dummyhdr={'Host':'www.blabla.com','Connection':'Keep-Alive'}
+            responseID = h2conn.request('GET',url=extractHeader.extract_GET_path(txn_req_headers),
+                                    headers=txn_req_headers_dict)
+            print("get response", responseID)
             return responseID
             #request_IDs.append(responseID)
             #response = h2conn.get_response(id)
@@ -148,7 +150,7 @@ def session_replay(input, proxy, result_queue):
                 for id in request_IDs:
                     print("extracting",id)
                     response = h2conn.get_response(id)
-                    print("code{0}:{1}".format(response.status,response.headers))
+                    print("code {0}:{1}".format(response.status,response.headers))
 
         bSTOP = True
         print("Queue is empty")
