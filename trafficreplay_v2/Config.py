c920493a7bd0019963c246956582b23a2029fc13
Syeda Persia Aziz <persia.aziz@yahoo.com>
2017-03-21 18:08:17 -0500

    config.py

---
+++ trafficreplay_v2/Config.py
@@ -0,0 +1,14 @@
+#SSL config
+ca_certs = "/home/persia/server.crt"
+keyfile = "/home/persia/server.key"
+
+#Proxy config
+proxy_host = "127.0.0.1"
+proxy_ssl_port = 443
+proxy_nonssl_port = 8080
+
+#process and thread config
+nProcess = 4
+nThread = 4
+
+
